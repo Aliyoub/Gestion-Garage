c321b58fb2ab46fefa750dbc798d4c8e285d36df
unknown <kibmimi>
2017-01-30 14:01:47 +0100

    Gestion de garage

--- LifeGarage.java
+++ LifeGarage.java
@@ -5,49 +5,55 @@ import java.util.ArrayList;
 
 public class LifeGarage {
 
-	public static void main(String[] args) {		
+	public static void main(String[] args) {
 		// Ajout et affichage de la voiture Renault
-		Car renault1 = new Car("renault",Color.RED, 999.9f); // Création (instanciation de la voiture)
-				
-		List<Car> cars = new ArrayList<>();		
+		Car renault1 = new Car("renault", Color.RED, 999.9f); // Création
+																// (instanciation
+																// de la
+																// voiture)
+
+		List<Car> cars = new ArrayList<>();
 		cars.add(renault1);
-		System.out.println(renault1.getBrand()+' '+renault1.getColor()+' '+renault1.getPrice() + "\n");
-		
+		System.out.println(renault1.getBrand() + ' ' + renault1.getColor() + ' ' + renault1.getPrice() + "\n");
+
 		// Ajout et affichage des voitures Peugeot, Volkswagen, Renault
-		Car peugeot1 = new Car("peugeot",Color.GREEN, 1000);
-		Car volkswagen1 = new Car("volkswagen",Color.BLUE, 1500);
-		Car renault2 = new Car("renault",Color.RED, 2000.5f);
-		
+		Car peugeot1 = new Car("peugeot", Color.GREEN, 1000);
+		Car volkswagen1 = new Car("volkswagen", Color.BLUE, 1500);
+		Car renault2 = new Car("renault", Color.RED, 2000.5f);
+
 		cars.add(peugeot1);
 		cars.add(volkswagen1);
 		cars.add(renault2);
-		
-		Garage garage = new Garage("garage"); // 
-		garage.addCar(cars);	// Ajout des voitures dans le garage		
- 
-		for(Car car : cars){
-			System.out.println(car.getBrand()+' '+car.getColor()+' '+car.getPrice()); // Affichage effective des voitures
+
+		Garage garage = new Garage("garage"); //
+		garage.addCar(cars); // Ajout des voitures dans le garage
+
+		for (Car car : cars) {
+			System.out.println(car.getBrand() + ' ' + car.getColor() + ' ' + car.getPrice()); // Affichage
+																								// effective
+																								// des
+																								// voitures
 		}
-		
-		System.out.println("\n /*************	Partie Abstrait	***********************************/");		
+
+		System.out.println("\n /*************	Partie Abstrait	***********************************/");
 		List<Vehicle> vehicles = new ArrayList<Vehicle>();
-		
+
 		// Ajout des véhicules dans le garage
-		vehicles.add(new Car("renault",Color.RED, 999.9f));
-		
-		vehicles.add(new Car("peugeot",Color.GREEN, 1000));
-		vehicles.add(new Car("volkswagen",Color.BLUE, 1500));
-		vehicles.add(new Car("renault",Color.RED, 2000.5f));		
-		
-		vehicles.add(new Bike("harley",Color.RED, 800f));		
+		vehicles.add(new Car("renault", Color.RED, 999.9f));
+
+		vehicles.add(new Car("peugeot", Color.GREEN, 1000));
+		vehicles.add(new Car("volkswagen", Color.BLUE, 1500));
+		vehicles.add(new Car("renault", Color.RED, 2000.5f));
+
+		vehicles.add(new Bike("harley", Color.RED, 800f));
 		vehicles.add(new Bike("ducati", Color.BLUE, 1000f));
 		vehicles.add(new Bike("bmw", Color.GREEN, 1999.9f));
-				
-		garage.addVehicle(vehicles);		
-		
+
+		garage.addVehicle(vehicles);
+
 		// Affichage des véhicules dans le garage avec leurs prix taxés
-		for(Vehicle vehicle : vehicles){
+		for (Vehicle vehicle : vehicles) {
 			System.out.println(vehicle.toString());
-		}				
+		}
 	}
 }
